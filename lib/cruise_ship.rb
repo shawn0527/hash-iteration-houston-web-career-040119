61855a611a3146f2616e3eefd2bb7367bdd7cd8c
Shawn Ren <doggy-ren@hotmail.com>
2019-03-09 02:53:39 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,9 +8,10 @@
 
 def select_winner(passengers)
   passengers.each do |suite, name|
-    if suite == suite_a && name.start_with("A")
+    if suite == suite_a && name.start_with?("A")
       winner = name
     end
+  end
     winner
   # add the code snippet here!
 end
